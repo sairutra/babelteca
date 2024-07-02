755a95292f71467e5247de46b987e5cf0ab01c0f
sebastianpenning <whimizera@mailbox.org>
2024-07-02 22:30:49 +0200

    added to books + documentation

--- docs/software/development/documentation.md
+++ docs/software/development/documentation.md
@@ -1,3 +1,4 @@
+https://medium.com/@techworldwithmilan/mastering-the-art-of-software-architecture-documentation-6f7e116e63fe
 https://www.doxygen.nl/manual/starting.html
 https://code.visualstudio.com/docs/cpp/cpp-ide
 https://www.youtube.com/results?search_query=doxygen+tutorial
--- docs/software/media/books.c
+++ docs/software/media/books.c
@@ -1,1 +1,28 @@
+https://blog.pragmaticengineer.com/my-reading-list/
+https://medium.com/@techworldwithmilan/books-every-software-engineer-must-read-in-2024-b992f2cea68d
+
+The Pragmatic Programmer by David Thomas and Andrew Hunt.
+Modern Software Engineering by David Farley
+Clean Code by Uncle Bob Martin.
+Head First Design Patterns by Eric Freeman.
+Refactoring by Martin Fowler.
+Grokking Algorithms by Aditya Bhargava.
+Introduction to Algorithms, by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein
+Designing Data-Intensive Applications by Martin Kleppman.
+Growing OO Software by Tests by Steve Freeman.
+Unit Testing Principles, Practices, and Patterns by Vladimir Khorikov.
+The Art of Unit Testing by Roy Osherove.
+TDD by Example by Kent Beck.
+Fundamentals Of Software Architecture by Mark Richards and Neil Ford.
+Clean Architecture by Uncle Bob Martin.
+Software Architecture The Hard Parts by Neal Ford, Mark Richards, Pramod Sadalage, and Zhamak Dehghani.
+Domain Driven Design Quickly by Abel Avram and Floyd Marinescu (Free E-book).
+A Philosophy of Software Design by John Ousterhout.
+Understanding Distributed Systems by Roberto Vitillo.
+DevOps Handbook by Gene Kim, Patrick Debois, John Willis, and Jez Humble.
+Continuous Delivery by Jez Humble and David Farley.
+The Hundred-Page Machine Learning Book by Andriy Burkov
+Code Complete: A Practical Handbook of Software Construction by Steve McConnell
+Software Engineering at Google — by Titus Winters, Tom Manshreck, and Hyrum Wright (Free Ebook)
+Clean Code by Uncle Bob Martin.
 The Art of Doing Science and Engineering: Learning to 
